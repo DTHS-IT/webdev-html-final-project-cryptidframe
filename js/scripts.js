a67e4a5f90048acecbc67734b205cfb5b514eeaf
KevinO-Delcastle <99449@students.nccvt.k12.de.us>
2025-10-29 12:38:24 +0000

    Finished Pop Up for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const modal = document.getElementById("download-innerContent");
-    const openBtn = document.getElementById("download-button");
-    const closeBtn = document.getElementById("close-button");
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.querySelector('.download-innerContent');
+    const openBtn = document.querySelector('.download-button');
+    const closeBtn = document.querySelector('.close-button');
 
-    openBtn = addEventListener("click", () => {
-        modal.classList.add("open");
-    });
+    if (openBtn && closeBtn && modal) {
+        openBtn.addEventListener('click', () => {
+            modal.classList.add('open');
+        });
 
-    closeBtn = addEventListener("click", () => {
-        modal.classList.remove("open");
-    });
+        closeBtn.addEventListener('click', () => {
+            modal.classList.remove('open');
+        });
+    }
 });
